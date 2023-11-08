08d00e8913182b00c1671a6bafd5caf87d11ed98
Alexandra Lignell <alexandra.lignell@elev.ga.ntig.se>
2023-11-08 11:07:57 +0100

    börjat lägga in lite grejer från top down spelet

--- Projekt_1/Program.cs
+++ Projekt_1/Program.cs
@@ -15,3 +15,74 @@
 // dörr rektanglar vid utgångar av labyrint som ändrar scen så det ser ut som att
 // man går genom korridoren till nästa rum liksom. Pokal i man hämtar i slutet 
 // eller nåt för att vinna.
+
+
+using Raylib_cs;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+
+Raylib.InitWindow(800, 600, "screen");
+Raylib.SetTargetFPS(60);
+
+
+Vector2 movement = new Vector2(0,0);
+
+
+string scene = "start";
+int hp = 3;
+float speed = 5;
+
+
+
+while (!Raylib.WindowShouldClose())
+{
+    if (scene == "start")
+    {
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+        {
+            scene = "gröntRum";
+        }
+    }
+    else if (scene == "gröntRum")
+    {
+        movement = Vector2.Zero;
+
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+        {
+            movement.X = 1;
+        }
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+        {
+            movement.X = -1;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+        {
+            movement.Y = -1;
+        }
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+        {
+            movement.Y = 1;
+        }
+        if (movement.Length() > 0)
+        {
+            movement = Vector2.Normalize(movement) * speed;
+        }
+        characterRect.x += movement.X;
+        characterRect.y += movement.Y;
+    }
+
+
+Raylib.BeginDrawing();
+    if (scene == "start")
+    {
+        Raylib.ClearBackground(Color.BLACK);
+        Raylib.DrawText("Press SPACE to start", 200, 300, 30, Color.WHITE);
+    }
+    else if (scene == "gröntRum")
+    {
+        Raylib.DrawText($"{hp}", 10, 10, 10, Color.WHITE);
+        Raylib.ClearBackground(Color.GREEN);
+    }
+
+}
