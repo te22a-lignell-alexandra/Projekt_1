d198ef59f06bb878ab42ea87c0f362059ba1b508
Alexandra Lignell <alexandra.lignell@elev.ga.ntig.se>
2023-11-08 10:48:38 +0100

    lite anteckningar och idéer

--- Projekt_1/Program.cs
+++ Projekt_1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿// ändra hastigheten m *-1 för att flytta fiender fram och tillbaka mellan två punkter
+
+// om fiendePos.x = 300 eller om fiendePos.x = 100: (300 och 100 blir vändpunkterna)
+//      fiendeMovement = fiendeMovement*-1
+
+// (så blir det negativt och fienden byter riktning, +*-=- ,-*-=+)
+
+
+// variabel för start, om träffar fiende --> position = start (för karaktär och fiende)
+
+// rörelse med knapptryck räknas som input
+
+// Karaktär i labyrint, man kan inte gå igenom väggar, finns en fiende (minst)
+// i varje rum, man har tre liv, vid kollision m fiende liv-1 och pos = start, 
+// dörr rektanglar vid utgångar av labyrint som ändrar scen så det ser ut som att
+// man går genom korridoren till nästa rum liksom. Pokal i man hämtar i slutet 
+// eller nåt för att vinna.
