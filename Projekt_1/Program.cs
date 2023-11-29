6151d8591109ad738ad1f0a708854cdabf0dfd24
Alexandra Lignell <alexandra.lignell@elev.ga.ntig.se>
2023-11-29 12:16:48 +0100

    gjort metoder för nästan allt

--- Projekt_1/Program.cs
+++ Projekt_1/Program.cs
@@ -16,6 +16,9 @@
 // man går genom korridoren till nästa rum liksom. Pokal i man hämtar i slutet 
 // eller nåt för att vinna.
 
+// weapon, om man inte hämtar vapnet tar man skada av fienden, gdsjkf = false, om man tar den dödar
+// man fienden och tar inte skada, giufjkgr = true. ??- man måste döda fienden för att kunna vinna??
+
 
 using Raylib_cs;
 using System.ComponentModel;
@@ -27,23 +30,27 @@ Raylib.InitWindow(800, 600, "screen");
 Raylib.SetTargetFPS(60);
 
 
+
+
+
+
 Texture2D characterImage = Raylib.LoadTexture("papillon.png");
 Rectangle characterRect = new Rectangle(400, 300, 64, 64);
-Vector2 movement = new Vector2(0,0);
+Vector2 movement = new Vector2(0, 0);
 
-// -----------------------------------------
-// doors
+
+// LISTS
 List<Rectangle> doors = new();
-doors.Add(new Rectangle (0, 150, 10, 100));
+doors.Add(new Rectangle(0, 150, 10, 100));
 
-// -----------------------------------------
-// WALLS
+
 List<Rectangle> walls = new();
-walls.Add(new Rectangle (300, 0, 50, 200));
-walls.Add(new Rectangle (0, 300, 350, 50));
-walls.Add(new Rectangle (500, 0, 50, 300));
-// -----------------------------------------
+walls.Add(new Rectangle(300, 0, 50, 200));
+walls.Add(new Rectangle(0, 300, 350, 50));
+walls.Add(new Rectangle(500, 0, 50, 300));
 
+
+// VARIABLES
 string scene = "start";
 int hp = 3;
 float speed = 5;
@@ -52,110 +59,172 @@ float speed = 5;
 
 while (!Raylib.WindowShouldClose())
 {
-    // START SCREEN
     if (scene == "start")
     {
-        if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
-        {
-            scene = "gröntRum";
-        }
+        scene = Start(scene);
     }
-    // MOVEMENT
-    else if (scene !="start")
+
+    
+    else if (scene != "start")
     {
         movement = Vector2.Zero;
+        movement = Movement(movement, speed);
 
-        if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
-        {
-            movement.X = -1;
-        }
-        if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
-        {
-            movement.X = 1;
-        }
-        if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
-        {
-            movement.Y = -1;
-        }
-        if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
-        {
-            movement.Y = 1;
-        }
-        if (movement.Length() > 0)
-        {
-            movement = Vector2.Normalize(movement) * speed;
-        }
 
         // CANT MOVE OUTSIDE OF SCREEN OR THROUGH WALLS
-        // --------------------------------------------------------------------------------
         characterRect.X += movement.X;
-        foreach (Rectangle wall in walls)
-        {
-            if (Raylib.CheckCollisionRecs(characterRect, wall))
-            {
-                characterRect.X -= movement.X;
-            }
-        }
+        if (CollidesWithWalls(characterRect, walls)) characterRect.X -= movement.X;
 
         characterRect.Y += movement.Y;
-        foreach (Rectangle wall in walls)
-        {
-            if (Raylib.CheckCollisionRecs(characterRect, wall))
-            {
-                characterRect.Y -= movement.Y;
-            }
-        }
-        if (characterRect.X > 800 - characterRect.Width || characterRect.X < 0)
-            {
-                characterRect.X -= movement.X;
-            }
-        if (characterRect.Y > 600 - characterRect.Height || characterRect.Y < 0)
-            {
-                characterRect.Y -= movement.Y;
-            }
-        // DÖRRAR
-        // -------------------------------------------------------------------------
+        if (CollidesWithWalls(characterRect, walls)) characterRect.Y -= movement.Y;
+
+        if (CollidesWithEdgeX(characterRect)) characterRect.X -= movement.X;
+        if (CollidesWithEdgeY(characterRect)) characterRect.Y -= movement.Y;
+
+        // DOORS
         if (Raylib.CheckCollisionRecs(characterRect, doors[0]))
         {
-            scene = "lilarum";
+            scene = "lilaRum";
         }
     }
+
+
     // -----------------------------------------------------------------------------
     // DRAW
     // -----------------------------------------------------------------------------
+
+
     Raylib.BeginDrawing();
-    // START SCREEN
-        if (scene == "start")
-        {
-            Raylib.ClearBackground(Color.BLACK);
-            Raylib.DrawText("Press SPACE to start", 200, 300, 30, Color.WHITE);
-        }
-        // RUM 1
-        else if (scene == "gröntRum")
-        {
-            Raylib.ClearBackground(Color.GREEN);
-            Raylib.DrawText($"HP: {hp}", 20, 20, 30, Color.WHITE);
-            Raylib.DrawTexture(characterImage, (int)characterRect.X, (int)characterRect.Y, Color.WHITE);
-            
-            foreach (Rectangle wall in walls)
-            {
-                Raylib.DrawRectangleRec(wall, Color.GOLD);
-            }
-            foreach (Rectangle door in doors)
-            {
-                Raylib.DrawRectangleRec(door, Color.DARKPURPLE);
-            }
-        }
-        else if (scene == "lilarum")
-        {
-            Raylib.ClearBackground(Color.DARKPURPLE);
-            Raylib.DrawText($"HP: {hp}", 20, 20, 30, Color.WHITE);
-            Raylib.DrawTexture(characterImage, (int)characterRect.X, (int)characterRect.Y, Color.WHITE);
-            
-            foreach (Rectangle wall in walls)
-            {
-                Raylib.DrawRectangleRec(wall, Color.BLACK);
-            }
-        }
+ 
+    if (scene == "start")
+    {
+        Raylib.ClearBackground(Color.BLACK);
+        DrawStartText();
+    }
+
+    // ROOM 1
+    else if (scene == "gröntRum")
+    {
+        DrawRoom(characterImage, characterRect, doors, walls, hp, Color.GREEN, Color.GOLD, Color.DARKPURPLE);
+    }
+    // ROOM 2
+    else if (scene == "lilaRum")
+    {
+        DrawRoom(characterImage, characterRect, doors, walls, hp, Color.DARKPURPLE, Color.BLACK, Color.WHITE);
+    }
+    // För olika dörrar eller väggar synliga i olika rum??
+
     Raylib.EndDrawing();
+}
+
+
+
+// ------------------------------------------------------------------------------------
+// METHODS
+
+
+// ------------------------------------COLLISIONS-------------------------------------------
+bool CollidesWithEdgeY(Rectangle characterRect)
+{
+    if (characterRect.Y > 600 - characterRect.Height || characterRect.Y < 0)
+        {
+            return true;
+        }
+
+    return false;
+}
+
+bool CollidesWithEdgeX(Rectangle characterRect)
+{
+    if (characterRect.X > 800 - characterRect.Width || characterRect.X < 0)
+        {
+            return true;
+        }
+
+    return false;
+}
+
+bool CollidesWithWalls(Rectangle characterRect, List<Rectangle> walls)
+{
+    foreach (Rectangle wall in walls)
+    {
+        if (Raylib.CheckCollisionRecs(characterRect, wall))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// -----------------------------------DRAW-----------------------------------
+static void DrawHp(int hp)
+{
+    Raylib.DrawText($"HP: {hp}", 20, 20, 30, Color.WHITE);
+}
+
+static void DrawStartText()
+{
+    Raylib.DrawText("Press SPACE to start", 200, 300, 30, Color.WHITE);
+}
+
+static void DrawCharacter(Texture2D characterImage, Rectangle characterRect)
+{
+    Raylib.DrawTexture(characterImage, (int)characterRect.X, (int)characterRect.Y, Color.WHITE);
+}
+
+static void DrawRoom(Texture2D characterImage, Rectangle characterRect, List<Rectangle> doors, List<Rectangle> walls, int hp, Color bkgColor, Color wallColor, Color doorColor)
+{
+    Raylib.ClearBackground(bkgColor);
+    DrawHp(hp);
+    DrawCharacter(characterImage, characterRect);
+
+    foreach (Rectangle wall in walls)
+    {
+        Raylib.DrawRectangleRec(wall, wallColor);
+    }
+
+    foreach (Rectangle door in doors)
+    {
+        Raylib.DrawRectangleRec(door, doorColor);
+    }
+}
+
+// ---------------------------MOVEMENT-----------------------------------------
+static Vector2 Movement(Vector2 movement, float speed)
+{
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+    {
+        movement.X = -1;
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+    {
+        movement.X = 1;
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+    {
+        movement.Y = -1;
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+    {
+        movement.Y = 1;
+    }
+    if (movement.Length() > 0)
+    {
+        movement = Vector2.Normalize(movement) * speed;
+    }
+
+    return movement;
+}
+
+
+// --------------------------------START SCREEN----------------------------
+static string Start(string scene)
+{
+    if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+    {
+        scene = "gröntRum";
+    }
+
+    return scene;
 }
